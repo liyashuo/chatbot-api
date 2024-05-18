cd16b0021e785c2f52c3cb6bea908b8406e0172f
yaron_lee <742847901@qq.com>
2024-05-18 10:34:50 +0800

    测试知识星球接口

--- chatbot-api-interfaces/src/test/java/cn/bugstack/chatbot/api/test/ApiTest.java
+++ chatbot-api-interfaces/src/test/java/cn/bugstack/chatbot/api/test/ApiTest.java
@@ -33,12 +33,14 @@ public class ApiTest {
     public void query_unanswered_questions() throws IOException {
         CloseableHttpClient httpClient = HttpClientBuilder.create().build();
 
-        HttpGet get = new HttpGet("https://api.zsxq.com/v2/groups/48411118851818/topics?scope=unanswered_questions&count=20");
+        HttpGet get = new HttpGet("https://api.zsxq.com/v2/groups/88888255114512/topics?scope=unanswered_questions&count=20");
 
-        get.addHeader("cookie", "知识星球个人cookie信息");
+        //如果失败了需要随时更新cookie
+        get.addHeader("cookie", "abtest_env=product; zsxqsessionid=7a88d50b5eb3e5d9d753c565cd92ce18; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22415485512584528%22%2C%22first_id%22%3A%2218d30ea766b582-037f55315fba542-26001951-2073600-18d30ea766cd93%22%2C%22props%22%3A%7B%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMThkMzBlYTc2NmI1ODItMDM3ZjU1MzE1ZmJhNTQyLTI2MDAxOTUxLTIwNzM2MDAtMThkMzBlYTc2NmNkOTMiLCIkaWRlbnRpdHlfbG9naW5faWQiOiI0MTU0ODU1MTI1ODQ1MjgifQ%3D%3D%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%24identity_login_id%22%2C%22value%22%3A%22415485512584528%22%7D%2C%22%24device_id%22%3A%2218d30ea766b582-037f55315fba542-26001951-2073600-18d30ea766cd93%22%7D; zsxq_access_token=8673600C-2539-A6ED-61E2-726C3F810E9F_4675DF4ECF5D1908");
         get.addHeader("Content-Type", "application/json;charset=utf8");
 
         CloseableHttpResponse response = httpClient.execute(get);
+
         if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
             String res = EntityUtils.toString(response.getEntity());
             System.out.println(res);
@@ -51,15 +53,17 @@ public class ApiTest {
     public void answer() throws IOException {
         CloseableHttpClient httpClient = HttpClientBuilder.create().build();
 
-        HttpPost post = new HttpPost("https://api.zsxq.com/v2/topics/412884248251548/answer");
-        post.addHeader("cookie", "知识星球个人cookie信息");
+        //"https://api.zsxq.com/v2/topics/{topic_id}/answer"
+        //根据get获取未回答列表，然后更新topic_id进行回答
+        HttpPost post = new HttpPost("https://api.zsxq.com/v2/topics/5122581121282884/answer");
+        post.addHeader("cookie", "abtest_env=product; zsxqsessionid=7a88d50b5eb3e5d9d753c565cd92ce18; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22415485512584528%22%2C%22first_id%22%3A%2218d30ea766b582-037f55315fba542-26001951-2073600-18d30ea766cd93%22%2C%22props%22%3A%7B%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMThkMzBlYTc2NmI1ODItMDM3ZjU1MzE1ZmJhNTQyLTI2MDAxOTUxLTIwNzM2MDAtMThkMzBlYTc2NmNkOTMiLCIkaWRlbnRpdHlfbG9naW5faWQiOiI0MTU0ODU1MTI1ODQ1MjgifQ%3D%3D%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%24identity_login_id%22%2C%22value%22%3A%22415485512584528%22%7D%2C%22%24device_id%22%3A%2218d30ea766b582-037f55315fba542-26001951-2073600-18d30ea766cd93%22%7D; zsxq_access_token=8673600C-2539-A6ED-61E2-726C3F810E9F_4675DF4ECF5D1908");
         post.addHeader("Content-Type", "application/json;charset=utf8");
 
         String paramJson = "{\n" +
                 "  \"req_data\": {\n" +
                 "    \"text\": \"自己去百度！\\n\",\n" +
                 "    \"image_ids\": [],\n" +
-                "    \"silenced\": false\n" +
+                "    \"silenced\": true\n" +
                 "  }\n" +
                 "}";
 
